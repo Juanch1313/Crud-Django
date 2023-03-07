a34960833ccfbb63b87ea5708d6c8cd3b62374e3
Jhonny <juanchitorodi1312@gmail.com>
2023-03-07 13:37:17 -0600

    Cambios

--- sistema/settings.py
+++ sistema/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^u)@vdh9&m-yio_mjkqp96188a4ql)zibsif!&a!tf6@y5ah=i'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
